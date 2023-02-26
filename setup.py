ec5c157581d1f6ce1e31e66cb2427005880cadc6
Ninzalo <ninzalogg@MacBook-Pro-Ninzalo.local>
2023-02-26 16:17:22 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bots_lib",
+    version="0.1",
+    url="https://github.com/Ninzalo/bots_lib",
+    license="MIT",
+    author="Ninzalo",
+    # description='Add static script_dir() method to Path',
+    packages=find_packages(),
+    zip_safe=False,
+)

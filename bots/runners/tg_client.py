6ee17e253b90705cf976f7199d24b1c3a17a838f
Ninzalo <ninzalogg@MacBook-Pro-Ninzalo.local>
2023-02-26 18:34:01 +0300

    Fixed sending message to server

--- bots/runners/tg_client.py
+++ bots/runners/tg_client.py
@@ -6,8 +6,12 @@ from bots.server.server_func import send_to_server
 
 
 class Tg_client:
-    def __init__(self, dispatcher: Dispatcher) -> None:
+    def __init__(
+        self, dispatcher: Dispatcher, local_ip: str, local_port: int
+    ) -> None:
         self._dp = dispatcher
+        self._local_ip = local_ip
+        self._local_port = local_port
         self._dp.callback_query_handler()(self.callback_message_handler)
         self._dp.message_handler()(self.message_handler)
         self._started = False
@@ -21,13 +25,21 @@ class Tg_client:
             messenger="tg",
             payload=str_to_dict(string=query.data),
         )
-        await send_to_server(message=message_struct)
+        await send_to_server(
+            message=message_struct,
+            local_ip=self._local_ip,
+            local_port=self._local_port,
+        )
 
     async def message_handler(self, message: types.Message) -> None:
         message_struct = Message_struct(
             user_id=message.from_id, messenger="tg", text=message.text
         )
-        await send_to_server(message=message_struct)
+        await send_to_server(
+            message=message_struct,
+            local_ip=self._local_ip,
+            local_port=self._local_port,
+        )
 
     async def test_messages_rate(self):
         self._started = True
@@ -35,7 +47,11 @@ class Tg_client:
             message_struct = Message_struct(
                 user_id=432672691, messenger="tg", text=f"{num}"
             )
-            await send_to_server(message=message_struct)
+            await send_to_server(
+                message=message_struct,
+                local_ip=self._local_ip,
+                local_port=self._local_port,
+            )
 
     def start_tg_client(self) -> None:
         if self._started:
@@ -48,15 +64,27 @@ class Tg_client:
         asyncio.run(self.test_messages_rate())
 
 
-def start_tg_client(dispatcher: Dispatcher) -> None:
-    tg_client = _get_tg_client(dispatcher=dispatcher)
+def start_tg_client(
+    dispatcher: Dispatcher, handler_ip: str, handler_port: int
+) -> None:
+    tg_client = _get_tg_client(
+        dispatcher=dispatcher, local_ip=handler_ip, local_port=handler_port
+    )
     tg_client.start_tg_client()
 
 
-def run_test(dispatcher: Dispatcher) -> None:
-    tg_client = _get_tg_client(dispatcher=dispatcher)
+def run_test(
+    dispatcher: Dispatcher, handler_ip: str, handler_port: int
+) -> None:
+    tg_client = _get_tg_client(
+        dispatcher=dispatcher, local_ip=handler_ip, local_port=handler_port
+    )
     tg_client.run_test()
 
 
-def _get_tg_client(dispatcher: Dispatcher) -> Tg_client:
-    return Tg_client(dispatcher=dispatcher)
+def _get_tg_client(
+    dispatcher: Dispatcher, handler_ip: str, handler_port: int
+) -> Tg_client:
+    return Tg_client(
+        dispatcher=dispatcher, local_ip=handler_ip, local_port=handler_port
+    )
--- bots/runners/vk_client.py
+++ bots/runners/vk_client.py
@@ -6,8 +6,10 @@ from bots.server.server_func import send_to_server
 
 
 class Vk_client:
-    def __init__(self, handler: Bot) -> None:
+    def __init__(self, handler: Bot, local_ip: str, local_port: int) -> None:
         self._bot = handler
+        self._local_ip = local_ip
+        self._local_port = local_port
         self._bot.on.raw_event(
             GroupEventType.MESSAGE_EVENT, dataclass=MessageEvent
         )(self.handle_callback_event)
@@ -19,7 +21,11 @@ class Vk_client:
         message = Message_struct(
             user_id=user_id, messenger="vk", payload=payload
         )
-        await send_to_server(message)
+        await send_to_server(
+            message=message,
+            local_ip=self._local_ip,
+            local_port=self._local_port,
+        )
 
     async def handle_message_event(self, event: Message):
         if event.payload:
@@ -32,13 +38,19 @@ class Vk_client:
             text=event.text,
             payload=payload,
         )
-        await send_to_server(message)
+        await send_to_server(
+            message=message,
+            local_ip=self._local_ip,
+            local_port=self._local_port,
+        )
 
     def start_vk_bot(self):
         print(f"VK listening started")
         self._bot.run_forever()
 
 
-def start_vk_client(handler: Bot):
-    vk_client = Vk_client(handler=handler)
+def start_vk_client(handler: Bot, handler_ip: str, handler_port: int):
+    vk_client = Vk_client(
+        handler=handler, local_ip=handler_ip, local_port=handler_port
+    )
     vk_client.start_vk_bot()

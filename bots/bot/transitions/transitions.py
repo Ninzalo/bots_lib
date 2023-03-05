a79ab571b1fdc2b7399406c7f8431926fd87d474
Ninzalo <ninzalogg@MacBook-Pro-Ninzalo.local>
2023-03-05 19:13:54 +0300

    Minor changes

--- bots/bot/transitions/transitions.py
+++ bots/bot/transitions/transitions.py
@@ -93,6 +93,7 @@ class Transitions:
             else_transition = await self._get_none_transition_by_stage(
                 stage=user_stage
             )
+            print(else_transition)
             print(inspect.getfullargspec(else_transition))
             if inspect.getfullargspec(else_transition)[0] == [
                 "user_messenger_id",

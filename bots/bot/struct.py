b64f207e29afb7f4975508ff018875d3790e1f30
Ninzalo <ninzalogg@MacBook-Pro-Ninzalo.local>
2023-03-11 19:55:25 +0300

    Minor changes

--- bots/bot/struct.py
+++ bots/bot/struct.py
@@ -4,14 +4,7 @@ from bots.base_config import BaseConfig
 
 @dataclass()
 class Message_struct:
-    def __init__(
-        self,
-        user_id: int,
-        messenger: BaseConfig.ADDED_MESSENGERS = BaseConfig.ADDED_MESSENGERS,
-        text: str | None = None,
-        payload: dict | None = None,
-    ) -> None:
-        self.user_id = user_id
-        self.text = text
-        self.payload = payload
-        self.messenger = messenger
+    user_id: int
+    messenger: BaseConfig.ADDED_MESSENGERS = BaseConfig.ADDED_MESSENGERS
+    text: str | None = None
+    payload: dict | None = None
